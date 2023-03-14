3e6614d7752a61ee19361f257e1748869885a717
wildanzr <graita.sukma@gmail.com>
2023-03-14 14:58:44 +0700

    feat: contact and social media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,13 @@ import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 export default function Home () {
   return (
-    <main className='h-full w-full flex flex-col px-5 py-5 font-poppins bg-snow dark:bg-main text-alternate  dark:text-snow duration-500 ease-out'>
+    <main className='h-full w-full flex flex-col px-5 py-5 font-poppins bg-snow dark:bg-main text-alternate dark:text-snow duration-500 ease-out'>
       <GoogleAnalytics gaMeasurementId="G-D39WFJ8Z2L" />
       <Navbar />
       <Hero />
       <About />
       <Stack />
       <Projects />
-      <div className="flex h-96"></div>
-      <div className="flex h-96"></div>
       <Contact />
     </main>
   )

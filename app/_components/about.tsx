43f64c742d75ae6efe358270f700d0e77068bf8a
Danzrrr <graita.sukma@gmail.com>
2025-08-07 23:32:14 +0700

    feat: base about

--- app/_components/about.tsx
+++ app/_components/about.tsx
@@ -6,7 +6,9 @@ const About = () => {
       id="about"
       className="flex flex-col items-center justify-center w-full h-full gap-5 lg:h-screen gradient-linear-reverse lg:flex-row lg:justify-between"
     >
-      <p>About</p>
+      <div className="container mx-auto p-5 w-full h-full grid grid-cols-1 lg:grid-cols-2 gap-10 lg:items-center lg:justify-center">
+        <p>About</p>
+      </div>
     </section>
   );
 };
